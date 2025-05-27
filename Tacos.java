37e6f48577f79ace3e711219792ceec798b75ce2
1915950 <1915950@01-18130.pusd.dom>
2025-05-27 13:23:04 -0700

    Started implementing ingredients

--- Tacos.java
+++ Tacos.java
@@ -1,9 +1,15 @@
 
-public class Tacos {
+public class Tacos extends Ingredients {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Tacos(boolean whiteRice, boolean noRice, boolean blackBeans, boolean pintoBeans, boolean veggies,
+			boolean beefB, boolean chicken, boolean sofritas, boolean carnitas, boolean mild, boolean spicy1,
+			boolean spicy2, boolean corn, boolean sourCream, boolean cheese, boolean guac, boolean lettuce,
+			boolean tortilla) {
+		super(whiteRice, noRice, blackBeans, pintoBeans, veggies, beefB, chicken, sofritas, carnitas, mild, spicy1, spicy2,
+				corn, sourCream, cheese, guac, lettuce, tortilla);
+		// TODO Auto-generated constructor stub
 	}
+
+	
 
 }
